6ca65e9c48d4da58087d090e18889bc5e991818a
dracc <ssrinet10@gmail.com>
2024-08-25 18:09:05 +0530

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,29 @@ import bcrypt from 'bcryptjs'
 import session from 'express-session'
 import {Strategy as LocalStrategy} from 'passport-local'
 import passport from 'passport'
+import dotenv from 'dotenv'
 import express from 'express'
 import userRouter from './routes/user'
 import authRouter from './routes/auth'
-
+import mongoose from 'mongoose'
+import {  ProcessEnv } from './types/environment'
 const app=express()
 
+dotenv.config({path:__dirname+'/.env'})
 const PORT=3000;
 
 app.set('view engine','pug')
+
+const mongoDBURI=process.env["MONGODB_URI"]
+if(!mongoDBURI)
+  {
+  throw "MonogdB empty";
+}
+
+mongoose.set("strictQuery",false)
+const main=async ()=>{
+  await mongoose.connect(mongoDBURI)
+}
 
 
 app.set('views',path.join(__dirname,'views'))
